851b99d4cecf1023ca166690f13a9109fb8cd52a
wkddntjr1123 <hbo1123@naver.com>
2021-06-02 18:44:40 +0900

    인식 테스트

--- opencv-face-recognition-python-master/OpenCV-Face-Recognition-Python.py
+++ opencv-face-recognition-python-master/OpenCV-Face-Recognition-Python.py
@@ -52,7 +52,8 @@ import numpy as np
 
 #there is no label 0 in our training data so subject name for index/label 0 is empty
 #근데 내 학번이 201515300이면 subjects[201515300]을 해야하는데
-subjects = ["", "JangWooSeok", "Elvis Presley"]
+subjects = [None]*1000000000
+#subjects = ["", "JangWooSeok", "Elvis Presley"]
 subjects[201515300] = "plz"
 
 # ### Prepare training data
