a53b74d74c8b5ae046443a22e1bd4d306497c4d4
wkddntjr1123 <hbo1123@naver.com>
2021-06-04 01:35:14 +0900

    asdf

--- professor/views.py
+++ professor/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render,redirect
 from .models import Lecture
 from authentication.models import User
 from student.models import Attendance
-import subprocess
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import imutils
+import pickle
+import time
+import cv2
 
 def manageAttendance(request, lecture_id, weekNum) :
     lecture = Lecture.objects.get(id=int(lecture_id)) #강의 객체
@@ -54,9 +60,81 @@ def 자동출석(request,recture_id,week) :
 '''
 
 def autoAttend(request) :
-    cmd = ['python3', '/home/pi/Desktop/smart_classroom/facial_recognition/facial_req.py']
-    fd_popen = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout 
-    data = fd_popen.read().strip()
-    print("@@@@@@@@@@@@@@@@@")
-    print(data)
+ 
+    currentname = "unknown"
+    encodingsP = "/home/pi/Desktop/smart_classroom/facial_recognition/encodings.pickle"
+    cascade = "/home/pi/Desktop/smart_classroom/facial_recognition/haarcascade_frontalface_default.xml"
+    
+    print("[INFO] loading encodings + face detector...")
+    data = pickle.loads(open(encodingsP, "rb").read())
+    detector = cv2.CascadeClassifier(cascade)
+        
+    print("[INFO] starting video stream...")
+    vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)
+    
+    fps = FPS().start()
+    
+    while True:
+    
+        frame = vs.read()
+        frame = imutils.resize(frame, width=500)
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+            minNeighbors=5, minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE)
+
+        boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
+
+        for encoding in encodings:
+            matches = face_recognition.compare_faces(data["encodings"],
+                encoding)
+            name = "Unknown"
+
+            if True in matches:
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+
+                name = max(counts, key=counts.get)
+                
+                if currentname != name:
+                    currentname = name
+                    ############culseok code#############
+                    print(currentname)
+            
+            names.append(name)
+
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            cv2.rectangle(frame, (left, top), (right, bottom),
+                (0, 255, 225), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                .8, (0, 255, 255), 2)
+
+        cv2.imshow("Facial Recognition is Running", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
+        fps.update()
+        
+        fps.stop()
+        print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+        # do a bit of cleanup
+        cv2.destroyAllWindows()
+        vs.stop()
+        
     return JsonResponse({"success":"fffds"})
