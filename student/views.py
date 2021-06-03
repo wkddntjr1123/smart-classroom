f15e6d88476e60cace38d49431b2fe727a6682b8
wkddntjr1123 <hbo1123@naver.com>
2021-06-03 21:43:47 +0900

    asdf

--- student/views.py
+++ student/views.py
@@ -78,7 +78,7 @@ def face_recognization (request) :
     # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
     # Get a reference to webcam #0 (the default one)
-    video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(-1)
     video_capture.set(3,320)
     video_capture.set(4,240)
     # Load a sample picture and learn how to recognize it.
