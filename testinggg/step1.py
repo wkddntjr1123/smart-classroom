6750cf56e4930cb14c072cea39e8859e5a8d4973
wkddntjr1123 <hbo1123@naver.com>
2021-06-03 23:51:16 +0900

    fff

--- testinggg/step1.py
+++ testinggg/step1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 # Cascades 디렉토리의 haarcascade_frontalface_default.xml 파일을 Classifier로 사용
-faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
